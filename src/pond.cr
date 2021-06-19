50a40dbd4db4fea84355317dcf0db1ee4ce25473
akadusei <attakusiadusei@gmail.com>
2021-06-19 20:36:57 +0000

    Ensure dead fibers continue to be removed whenever pond is refilled

--- src/pond.cr
+++ src/pond.cr
@@ -2,12 +2,16 @@ require "./pond/version"
 require "./pond/**"
 
 class Pond
-  def initialize(@fibers = Array(Fiber).new)
+  def initialize
+    @fibers = Array(Fiber).new
     @fiber = Fiber.current
     @mutex = Mutex.new
-    @done = false
+  end
 
-    remove_dead_fibers_async
+  def self.new(fibers)
+    pond = new
+    fibers.each { |fiber| pond.fill(fiber) }
+    pond
   end
 
   def <<(fiber)
@@ -19,16 +23,25 @@ class Pond
   end
 
   def fill(fiber : Fiber)
-    sync { @fibers << fiber }
+    sync do
+      @fibers << fiber
+      @done = false
+    end
+
+    remove_dead_fibers
     self
   end
 
   def drain : Nil
-    if @fiber != Fiber.current
-      raise Error.new("Cannot drain pond from another fiber")
+    ensure_same_fiber
+    return unless @done == false
+
+    until @fibers.empty?
+      Fiber.yield
+      sync { @fibers.reject!(&.dead?) }
     end
 
-    remove_dead_fibers
+    sync { @done = nil } unless @done
 
     until @done
       Fiber.yield
@@ -45,18 +58,24 @@ class Pond
     new(fibers).drain
   end
 
-  private def remove_dead_fibers_async
+  private def remove_dead_fibers
+    return unless size == 1
+
     spawn do
-      remove_dead_fibers
-      @done = true
+      until @done.nil?
+        sync { @fibers.reject!(&.dead?) }
+        Fiber.yield
+      end
+
+      sync { @done = true }
     end
   end
 
-  private def remove_dead_fibers
-    until @fibers.empty?
-      Fiber.yield
-      sync { @fibers.reject!(&.dead?) }
-    end
+  private def ensure_same_fiber
+    return if @fiber == Fiber.current
+
+    sync { @done = nil }
+    raise Error.new("Cannot drain pond from another fiber")
   end
 
   private def sync
