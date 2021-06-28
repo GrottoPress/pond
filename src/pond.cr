GrottoPress/pond

commit 4e8bc09e5e3750f52367549e99fdc705f27cdcac

Exclude dead fibers from `Pond#size`

src/pond.cr
@@ -39,9 +39,8 @@ class Pond
     ensure_same_fiber
     return unless @done == false
 
-    until @fibers.empty?
+    until @fibers.all?(&.dead?)
       Fiber.yield
-      sync { @fibers.reject!(&.dead?) }
     end
 
     sync { @done = nil } unless @done
@@ -51,7 +50,9 @@ class Pond
     end
   end
 
-  delegate :size, to: @fibers
+  def size
+    @fibers.count(&.dead?.!)
+  end
 
   def self.drain(fiber : Fiber)
     drain([fiber])

commit ad160fe919d7601b2ed8846e01e066c97a8be68e

Fix segfault while removing dead fibers
Probably a regression in 4e8bc09e5e3750f52367549e99fdc705f27cdcac.

The idea of checking `size == 1` in `#remove_dead_fibers` was to
ensure only one 'remove_dead_fibers' fiber is spawned until pond
is drained.

But because `Pond#size` does not consider dead fibers, there could
be a situation where multiple 'remove_dead_fibers' fibers are
spawned, if a new fiber added is the only alive fiber.

If one of the 'remove_dead_fibers' fibers sets `done` to `true`,
the main thread could exit with other 'remove_dead_fibers' fibers
still running, which, I believe, leads to the segfault.

See https://github.com/GrottoPress/mel/runs/2932323566?check_suite_focus=true

src/pond.cr
@@ -63,7 +63,7 @@ class Pond
   end
 
   private def remove_dead_fibers
-    return unless size == 1
+    return unless @fibers.size == 1
 
     spawn do
       until @done.nil?